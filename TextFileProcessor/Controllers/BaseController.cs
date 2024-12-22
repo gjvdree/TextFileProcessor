a355746aac93f112ea8bf5c3dfb04647bc108c5b
Gertjan van de Ree <Gertjan@reedev.nl>
2024-12-22 14:06:35 +0100

    Add controller

---
+++ TextFileProcessor/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace TextFileProcessor.Web.Controllers
+{
+
+    [Route("api/[controller]")]
+    public abstract class BaseApiController : ApiController
+    {
+    }
+}
---
+++ TextFileProcessor/Controllers/TextFileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TextFileProcessor.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextFileController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> ModifyTextFile(IFormFile textFile)
+        {
+            if (textFile == null || textFile.Length == 0)
+                return BadRequest("Invalid file uploaded");
+
+            // Persist the file temporary
+            string tempFilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            using (FileStream stream = new(tempFilePath, FileMode.Create))
+            {
+                await textFile.CopyToAsync(stream);
+            }
+
+            //TODO Do stuff with the file
+
+            return File(System.IO.File.OpenRead(tempFilePath), "application/text", $"response_{DateTime.UtcNow:yyyyMMddTHHmmss}.txt"); // returns a FileStreamResult
+        }
+    }
+}
